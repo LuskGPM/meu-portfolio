f48a400b16d82dd93919d8d7fc11cfcb8f448e38
LuskGPM <145815917+LuskGPM@users.noreply.github.com>
2025-06-02 21:10:07 -0300

    alterações (por enquanto) finais

---
+++ script/script.js
@@ -0,0 +1,21 @@
+const sanduiche = document.getElementById('sanduiche');
+const navbar = document.getElementById('navbar');
+const header = document.getElementById('header');
+
+sanduiche.onclick = () => {
+    if (navbar.classList.contains('display')) {
+        navbar.classList.remove('display');
+        sanduiche.innerHTML = 'close';
+    } else {
+        navbar.classList.add('display');
+        sanduiche.innerHTML = 'menu';
+    }
+}
+
+window.onscroll = () => {
+    if (window.scrollY > 90) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+}
